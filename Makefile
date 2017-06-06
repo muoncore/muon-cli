c615a2f4efec461b33b55de33f0b1d5c553df657
David Dawson <me@daviddawson.me>
2017-06-06 10:39:29 +0100

    Update doc, publish snaps, babel transpile.

--- Makefile
+++ Makefile
@@ -4,5 +4,15 @@ ifndef VERSION
 	$(error VERSION is undefined for NPM release)
 endif
 	npm install
+	npm run build
 	npm version --no-git-tag-version $(VERSION)
 	npm publish
+
+publish-snapshot:
+	npm install
+	npm run build
+	npm version --no-git-tag-version prerelease
+	npm publish --tag next
+	git add package.json
+	git commit -m "Update snapshot version"
+	git push origin
--- cli/muon-cli.js
+++ cli/muon-cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 var muoncore = require('muon-core');
+
+require("muon-amqp").attach(muoncore)
+
 var cli = require('cli').enable('status');
 require('sexylog');
 var url = require('url');
@@ -22,74 +25,77 @@ var amqpUrl = process.env.MUON_URL;
 var cliobj;
 
 cli.parse({
-        // discover:   ['d', 'discover muon services'],
-      raw: ['r', "Output raw, no decoration. Suitable for post processing"]
-    },
-    {
-        "introspect":"Obtain introspection information for a service, showing implemented protocols and endpoints",
-        "discover":"Show the currently running services",
-        "rpc":"<url> <payload>",
-        "stream":"<url> [configuration]>",
-        "emit":"<event>",
-        "replay":"<stream name> [configuration]"
-    });
+    // discover:   ['d', 'discover muon services'],
+    raw: ['r', "Output raw, no decoration. Suitable for post processing"]
+  },
+  {
+    "introspect": "Obtain introspection information for a service, showing implemented protocols and endpoints",
+    "discover": "Show the currently running services",
+    "rpc": "<url> <payload>",
+    "stream": "<url> [configuration]>",
+    "emit": "<event>",
+    "replay": "<stream name> [configuration]"
+  });
 
-cli.main(function(args, options) {
-    cliobj = this;
+cli.main(function (args, options) {
+  cliobj = this;
 
-    var validAmqpUrl =  Joi.validate(amqpUrl,  Joi.string().uri().required());
+  var validAmqpUrl = Joi.validate(amqpUrl, Joi.string().uri().required());
 
-    if (validAmqpUrl.error) {
-        logger.error('amqp env variable url invalid! MUON_URL=' + amqpUrl);
-        logger.error('export MUON_URL=amqp://muon:microservices@localhost:5672');
-        exit();
-    }
-    // cliobj.ok('muon cli connected: ' + amqpUrl);
-    logger.trace("rpc() args=" + JSON.stringify(args));
+  if (validAmqpUrl.error) {
+    logger.error('amqp env variable url invalid! MUON_URL=' + amqpUrl);
+    logger.error('export MUON_URL=amqp://muon:microservices@localhost:5672');
+    exit();
+  }
+  // cliobj.ok('muon cli connected: ' + amqpUrl);
+  logger.trace("rpc() args=" + JSON.stringify(args));
 
-    switch(cli.command) {
-        case "discover":
-            withMuon(function (muon) {
-                builtin.discover(muon, options);
-            })
+  switch (cli.command) {
+    case "discover":
+      withMuon(function (muon) {
+        builtin.discover(muon, options);
+      })
 
-            break;
-        case "introspect":
-            withMuon(function (muon) {
-                builtin.introspect(muon, options, args);
-            })
-            break;
-        case "rpc":
-            withMuon(function (muon) {
-                rpc(muon, options, args);
-            })
-            break;
-        case "stream":
-            withMuon(function (muon) {
-                stream.stream(muon, options, args);
-            })
-            break;
-        case "replay":
-            withMuon(function (muon) {
-                stream.replay(muon, options, args);
-            })
-            break;
-        case "emit":
-            withMuon(function (muon) {
-                event.emit(muon, options, args);
-            })
+      break;
+    case "introspect":
+      withMuon(function (muon) {
+        builtin.introspect(muon, options, args);
+      })
+      break;
+    case "rpc":
+      withMuon(function (muon) {
+        rpc(muon, options, args);
+      })
+      break;
+    case "stream":
+      withMuon(function (muon) {
+        stream.stream(muon, options, args);
+      })
+      break;
+    case "replay":
+      withMuon(function (muon) {
+        stream.replay(muon, options, args);
+      })
+      break;
+    case "emit":
+      withMuon(function (muon) {
+        event.emit(muon, options, args);
+      })
 
-            break;
-        default:
-            logger.error("Unknown command " + cli.command);
-    }
+      break;
+    default:
+      logger.error("Unknown command " + cli.command);
+  }
 });
 
 function withMuon(exec) {
-    var muon = muoncore.create(cliName, amqpUrl);
-    exec(muon)
+  var muon = muoncore.create(cliName, amqpUrl);
+  require("muon-stack-event").create(muon)
+  require("muon-stack-rpc").create(muon)
+  require("muon-stack-reactive-streams").create(muon)
+  exec(muon)
 }
 
 function exit() {
-    process.exit(0);
+  process.exit(0);
 }
